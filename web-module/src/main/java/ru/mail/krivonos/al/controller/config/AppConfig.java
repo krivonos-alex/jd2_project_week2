67f0fa6210e8840a66f89a32f1c5716a9b74c328
krivonos-alex <krivonos-al@mail.ru>
2019-04-12 16:40:05 +0300

    Imported RepositoryConfig into AppConfig.

--- web-module/src/main/java/ru/mail/krivonos/al/controller/config/AppConfig.java
+++ web-module/src/main/java/ru/mail/krivonos/al/controller/config/AppConfig.java
@@ -2,9 +2,10 @@ package ru.mail.krivonos.al.controller.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import ru.mail.krivonos.al.repository.config.RepositoryConfig;
 import ru.mail.krivonos.al.services.config.ServicesConfig;
 
 @Configuration
-@Import({ControllerConfig.class, ServicesConfig.class})
+@Import({ControllerConfig.class, ServicesConfig.class, RepositoryConfig.class})
 public class AppConfig {
 }

c48a6c2f2aeda4d9e82a42c2163ad0eeb6f73540
krivonos-alex <krivonos-al@mail.ru>
2019-04-17 12:16:50 +0300

    Minor changes in tests for DocumentDTOValidator, resource files, and test resource files. Added private constructor into ServiceErrorMessageConstants.

--- service-module/src/main/java/ru/mail/krivonos/al/service/constants/ServiceErrorMessageConstants.java
+++ service-module/src/main/java/ru/mail/krivonos/al/service/constants/ServiceErrorMessageConstants.java
@@ -2,9 +2,6 @@ package ru.mail.krivonos.al.service.constants;
 
 public class ServiceErrorMessageConstants {
 
-    private ServiceErrorMessageConstants() {
-    }
-
     public static final String NULL_DOCUMENT_ERROR_MESSAGE = "Received Document is null.";
 
     public static final String DOCUMENT_NOT_FOUND_ERROR_MESSAGE = "Document not found.";
